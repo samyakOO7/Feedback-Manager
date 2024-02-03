65b228fcd63ea0676535a56d4450c767749812d9
kesrishubham2510 <shubhamkeshari90@example.com>
2024-02-04 00:17:37 +0530

    * Added Feedback service * Added userId in AddFeedbackRequest

--- BE/feedbackregistry/src/main/java/com/techtitans/feedbackregistry/gateway/controllers/FeedbackController.java
+++ BE/feedbackregistry/src/main/java/com/techtitans/feedbackregistry/gateway/controllers/FeedbackController.java
@@ -3,15 +3,20 @@ package com.techtitans.feedbackregistry.gateway.controllers;
 import com.techtitans.feedbackregistry.core.constants.AppConstants;
 import com.techtitans.feedbackregistry.core.dtos.request.AddFeedbackRequest;
 import com.techtitans.feedbackregistry.core.dtos.response.AddFeedbackResponse;
+import com.techtitans.feedbackregistry.gateway.services.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(AppConstants.apiPathPrefix)
 public class FeedbackController {
 
+    @Autowired
+    private FeedbackService feedbackService;
+
     @PostMapping(AppConstants.addFeedbackPath)
     public ResponseEntity<AddFeedbackResponse> addFeedback(@RequestBody AddFeedbackRequest addFeedbackRequest){
-        return null;
+        return ResponseEntity.ok().body(feedbackService.addFeedback(addFeedbackRequest));
     }
 
     @GetMapping(AppConstants.readFeedbackPath+"/{feedbackId}")
